4709b95c58db692c4ea450d0e157275af38542d1
webagencyco <codewizardsuk@gmail.com>
2024-08-29 16:05:55 +0500

    additional

--- src/Pages/Calculator.jsx
+++ src/Pages/Calculator.jsx
@@ -9,54 +9,50 @@ const PPCoastingCalculator = () => {
   });
   const [results, setResults] = useState({});
 
-  const A12 = 0.02;
-  const B12 = 0.08;
-  const E14 = 640;
-  const D16 = 1;
-  const E15 = 780;
-  const E16 = 3000;
-  const E17 = 970;
-  const E18 = 3.2;
-  const E21 = 180;
-  const H21 = 155;
-  const E22 = 230;
-  const E23 = 205;
-  const E24 = 315;
-
-  const F16 =
-    Math.round(
-      inputs.readySizeX *
-        (inputs.readySizeY + 1.5) *
-        inputs.sides *
-        0.0000032 *
-        1000 *
-        100
-    ) / 100;
-  console.log("F16", F16);
-  const F14 = E14 + 5 + (E14 + 5) * A12;
-  const H14 = E14 + 25 + (E14 + 25) * A12;
-  const H15 = E15 + 5 + (E15 + 5) * A12;
-  const H16 = (F16 / 1000) * E16 * D16;
-  console.log("H16", H16);
-  const F18 = (E18 / inputs.ups) * inputs.sides;
-  console.log("F18", F18);
-  const F19 = inputs.readySizeX * 0.057;
-  console.log("F19", F19);
-  const E19 = (F19 / 1000) * E17 + 0.5;
-  console.log("E19", E19);
+  const [tableInputs, setTableInputs] = useState({
+    A12: 0.02,
+    B12: 0.08,
+    E14: 640,
+    D16: 100,
+    E15: 780,
+    E16: 3000,
+    E17: 970,
+    E18: 3.2,
+    E21: 180,
+    H21: 155,
+    E22: 230,
+    E23: 205,
+    E24: 315,
+  });
 
   const calculateResults = () => {
+    const { A12, B12, E14, D16, E15, E16, E17, E18, E21, H21, E22, E23, E24 } =
+      tableInputs;
+    const { readySizeX, readySizeY, sides, ups } = inputs;
+
+    const F16 =
+      Math.round(
+        readySizeX * (readySizeY + 1.5) * sides * 0.0000032 * 1000 * 100
+      ) / 100;
+    const F14 = E14 + 5 + (E14 + 5) * A12;
+    const H14 = E14 + 25 + (E14 + 25) * A12;
+    const H15 = E15 + 5 + (E15 + 5) * A12;
+    const H16 = (F16 / 1000) * E16 * (D16/100);
+    const F18 = (E18 / ups) * sides;
+    const F19 = readySizeX * 0.057;
+    const E19 = (F19 / 1000) * E17 + 0.5;
+
     const calculateJ = (weight, index) => {
-      return index === 0 || index === 1 || index === 2 || index === 5
-        ? (weight / 1000) * F14 + (H16 + F18 + E19)
-        : index === 3 || index === 4
-        ? (weight / 1000) * H14 + H16 + F18 + E19
-        : (weight / 1000) * H15 + H16 + F18 + E19;
+      if (index === 0 || index === 1 || index === 2 || index === 5) {
+        return (weight / 1000) * F14 + (H16 + F18 + E19);
+      } else if (index === 3 || index === 4) {
+        return (weight / 1000) * H14 + H16 + F18 + E19;
+      } else {
+        return (weight / 1000) * H15 + H16 + F18 + E19;
+      }
     };
 
-    const calculateI = (j) => {
-      return j * B12;
-    };
+    const calculateI = (j) => j * B12;
 
     const specifications = [
       { spec: "18,20,22,23,25,27" },
@@ -69,47 +65,53 @@ const PPCoastingCalculator = () => {
     ];
 
     const newResults = specifications.map((item, index) => {
-      let weight =
-        index === 0 || index === 3
-          ? (E21 / 1550) * inputs.readySizeX * (inputs.readySizeY + 1.5)
-          : index === 1
-          ? (H21 / 1550) * inputs.readySizeX * (inputs.readySizeY + 1.5)
-          : index === 2 || index === 4
-          ? (E22 / 1550) * inputs.readySizeX * (inputs.readySizeY + 1.5)
-          : index === 5
-          ? (E23 / 1550) * inputs.readySizeX * (inputs.readySizeY + 1.5)
-          : (E24 / 1550) * inputs.readySizeX * (inputs.readySizeY + 1.5);
-      console.log("weight", weight);
+      let weight;
+      if (index === 0 || index === 3) {
+        weight = (E21 / 1550) * readySizeX * (readySizeY + 1.5);
+      } else if (index === 1) {
+        weight = (H21 / 1550) * readySizeX * (readySizeY + 1.5);
+      } else if (index === 2 || index === 4) {
+        weight = (E22 / 1550) * readySizeX * (readySizeY + 1.5);
+      } else if (index === 5) {
+        weight = (E23 / 1550) * readySizeX * (readySizeY + 1.5);
+      } else {
+        weight = (E24 / 1550) * readySizeX * (readySizeY + 1.5);
+      }
+
       const j = calculateJ(weight, index);
       const i = calculateI(j);
 
       return {
         specifications: item.spec,
-        rate: item.rate,
-        weight: weight,
-        i: i.toFixed(1),
-        j: index === 2 || index === 4 || index === 5 ? j + 2 : j,
+        rate: Math.ceil((i + j) / 0.5) * 0.5,
+        weight: weight.toFixed(0),
+        i: i.toFixed(2),
+        j: (index === 2 || index === 4 || index === 5 ? j + 2 : j).toFixed(2),
       };
     });
 
-    setResults({
-      table: newResults,
-    });
+    setResults({ table: newResults });
   };
 
   useEffect(() => {
     calculateResults();
-  }, [inputs]);
+  }, [inputs, tableInputs]);
 
   const handleInputChange = (e) => {
     const { name, value } = e.target;
     setInputs((prev) => ({ ...prev, [name]: parseFloat(value) }));
   };
 
+  const handleTableInputChange = (e) => {
+    const { name, value } = e.target;
+    setTableInputs((prev) => ({ ...prev, [name]: parseFloat(value) }));
+  };
+
   return (
     <div>
       <h2>PP Coasting Calculator</h2>
-      <div>
+
+      <div className="inputs">
         <div>
           <label htmlFor="readySizeX">Ready Size X</label>
           <input
@@ -163,29 +165,166 @@ const PPCoastingCalculator = () => {
             results.table.map((row, index) => (
               <tr key={index}>
                 <td>{row.specifications}</td>
-                <td>
-                  {Math.ceil((Number(row.i) + Number(row.j)) / 0.5) * 0.5}
-                </td>
-                <td>{Number(row.weight).toFixed(0)}</td>
-                <td>{Number(row.i).toFixed(2)}</td>
-                <td>{Number(row.j).toFixed(2)}</td>
+                <td>{row.rate}</td>
+                <td>{row.weight}</td>
+                <td>{row.i}</td>
+                <td>{row.j}</td>
               </tr>
             ))}
         </tbody>
       </table>
 
-      {/* <div>
-        <h2>Additional Results</h2>
-        <p>Rate PP: {results.ratePP}</p>
-        <p>Rate Craft: {results.rateCraft}</p>
-        <p>
-          Ink: {results.ink?.value1} / {results.ink?.value2}
-        </p>
-        <p>L/E: {results.le}</p>
-        <p>
-          Stitch: {results.stitch?.value1} / {results.stitch?.value2}
-        </p>
-      </div> */}
+      <div className="additional-inputs">
+        <div className="input-group">
+          <h3>Rate PP</h3>
+          <label>
+            E14:{" "}
+            <input
+              type="number"
+              name="E14"
+              value={tableInputs.E14}
+              onChange={handleTableInputChange}
+            />
+          </label>
+        </div>
+        <div className="input-group">
+          <h3>Rate Craft</h3>
+          <label>
+            E15:{" "}
+            <input
+              type="number"
+              name="E15"
+              value={tableInputs.E15}
+              onChange={handleTableInputChange}
+            />
+          </label>
+        </div>
+        <div className="input-group">
+          <h3>Ink</h3>
+          <label>
+            E16:{" "}
+            <input
+              type="number"
+              name="E16"
+              value={tableInputs.E16}
+              onChange={handleTableInputChange}
+            />
+          </label>
+          <label>
+            D16(percentage):{" "}
+            <input
+              type="number"
+              max={100}
+              min={0}
+              name="D16"
+              value={tableInputs.D16}
+              onChange={handleTableInputChange}
+            />
+          </label>
+        </div>
+        <div className="input-group">
+          <h3>Thread</h3>
+          <label>
+            E17:{" "}
+            <input
+              type="number"
+              name="E17"
+              value={tableInputs.E17}
+              onChange={handleTableInputChange}
+            />
+          </label>
+        </div>
+        <div className="input-group">
+          <h3>L/E</h3>
+          <label>
+            E18:{" "}
+            <input
+              type="number"
+              name="E18"
+              value={tableInputs.E18}
+              onChange={handleTableInputChange}
+            />
+          </label>
+        </div>
+        <div className="input-group">
+          <h3>PFN</h3>
+          <label>
+            E21:{" "}
+            <input
+              type="number"
+              name="E21"
+              value={tableInputs.E21}
+              onChange={handleTableInputChange}
+            />
+          </label>
+          <label>
+            H21:{" "}
+            <input
+              type="number"
+              name="H21"
+              value={tableInputs.H21}
+              onChange={handleTableInputChange}
+            />
+          </label>
+        </div>
+        <div className="input-group">
+          <h3>PPH</h3>
+          <label>
+            E22:{" "}
+            <input
+              type="number"
+              name="E22"
+              value={tableInputs.E22}
+              onChange={handleTableInputChange}
+            />
+          </label>
+        </div>
+        <div className="input-group">
+          <h3>PP H 23</h3>
+          <label>
+            E23:{" "}
+            <input
+              type="number"
+              name="E23"
+              value={tableInputs.E23}
+              onChange={handleTableInputChange}
+            />
+          </label>
+        </div>
+        <div className="input-group">
+          <h3>PP Craft</h3>
+          <label>
+            E24:{" "}
+            <input
+              type="number"
+              name="E24"
+              value={tableInputs.E24}
+              onChange={handleTableInputChange}
+            />
+          </label>
+        </div>
+        <div className="input-group">
+          <h3>Additional</h3>
+          <label>
+            A12:{" "}
+            <input
+              type="number"
+              name="A12"
+              value={tableInputs.A12}
+              onChange={handleTableInputChange}
+            />
+          </label>
+          <label>
+            B12:{" "}
+            <input
+              type="number"
+              name="B12"
+              value={tableInputs.B12}
+              onChange={handleTableInputChange}
+            />
+          </label>
+        </div>
+      </div>
     </div>
   );
 };
